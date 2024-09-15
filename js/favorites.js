990d18836471eb79c8142f46b0d29d55d2abd519
Pablo Perdigão <pabloagps@gmail.com>
2024-09-15 10:12:50 -0300

    Criando HTML com JavaScript

--- js/favorites.js
+++ js/favorites.js
@@ -1,26 +1,24 @@
 // class que vai conter a lógica dos dados
 // como os dados serão estruturados
-export class favorites {
+export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
   }
 
   load() {
-    this.entries = [
-      {
+    this.entries = [{
       login: 'pabloperdigao',
       name: "Pablo Perdigão",
       public_repos: '31',
       followers: '1'
     },
-
     {
       login: 'diego3g',
       name: "Diego Fernandes",
       public_repos: '71',
-      followers: '31200'
-    }
+      followers: '31.2k'
+    },
     ]
 
     
@@ -29,7 +27,7 @@ export class favorites {
 }
 
 // class que vi criar a visualzação e eventos do HTML 
-export class FavoritesView extends favorites {
+export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
@@ -42,19 +40,19 @@ export class FavoritesView extends favorites {
   update() {
     this.removeAllTr()
 
-      this.entries.forEach( user => {
-      const row = this.createRow()
+      this.entries.forEach(user => {
+        const row = this.createRow()
 
-      row.querySelector('user img').src = ` https://github.com/${user.login}.png`
+        row.querySelector('.user img').src = `https://github.com/${user.login}.png`
 
-      row.querySelector('user img').alt = `Imagem de ${user.name}` 
+        row.querySelector('.user img').alt = `Imagem de ${user.name}` 
 
-      row.querySelector('.user p').textContent = user.name
-      row.querySelector('.user span').textContent = user.login
+        row.querySelector('.user p').textContent = user.name
+        row.querySelector('.user span').textContent = user.login
 
-      row.querySelector('.repositores').textContent = user.public_repos
+        row.querySelector('.repositories').textContent = user.public_repos
 
-      row.querySelector('followers').textContent = user.followers
+        row.querySelector('.followers').textContent = user.followers
 
 
       this.tbody.append(row)
@@ -67,7 +65,6 @@ export class FavoritesView extends favorites {
     const tr = document.createElement('tr')
 
     tr.innerHTML = `
-      <tr>
         <td class="user">
           <img src="https://github.com/pabloperdigao.png" alt="Imagem do Pablo Perdigão">
           <a href="https://github.com/pabloperdigao" target="_blank">
@@ -86,16 +83,15 @@ export class FavoritesView extends favorites {
         <td>
           <button class="remove">&times;</button>
         </td>
-      </tr>
-    
     `
     return tr
   }
 
-  removeAllTr() {this.tbody.querySelectorAll('tr')
-       .forEach((tr) => {
-         tr.remove()
-       })
+  removeAllTr() {
+    
+    this.tbody.querySelectorAll('tr').forEach((tr)=> {
+     tr.remove()
+    });
   }
 
 }
